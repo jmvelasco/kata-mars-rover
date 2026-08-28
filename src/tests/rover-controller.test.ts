c64ad561e30d8dd2d2d3131c8c6b2fab0c007e1b
JManuel Velasco <jmanuel.velasco@gmail.com>
2026-08-29 00:54:40 +0200

    crear factory para el rover en su suite de tests

--- src/tests/rover-controller.test.ts
+++ src/tests/rover-controller.test.ts
@@ -1,52 +1,33 @@
 import { describe, expect, test } from '@jest/globals';
 import { RoverController } from '../core/rover-controller';
-import { Orientation } from '../core/orientation';
+import { Directions, Orientation } from '../core/orientation';
 import { Surface } from '../core/surface';
 import { Position } from '../core/position';
 
+const createRoverAt = (x: number, y: number, dir: Directions, surfaceRows = 10, surfaceCols = 20) => {
+  const surface = new Surface(surfaceRows, surfaceCols);
+  const orientation = Orientation.create(dir);
+  const position = Position.create(x, y, orientation);
+  return RoverController.initialize(position, surface);
+};
+
 describe('The RoverController', () => {
   test('should be initialised with a position inside the boundaries of the surface', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('N');
-    const position = Position.create(0, 0, orientation);
-    const rover = RoverController.initialize(position, surface);
+    const rover = createRoverAt(0, 0, 'N');
     expect(rover.displayPosition()).toBe('0:0:N');
   });
 
   test('should throw an error if it is positioned initially outside the boundaries of the surface', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('N');
-    expect(() => RoverController.initialize(Position.create(22, 0, orientation), surface)).toThrow(
-      'The Rover is positioned outside the boundaries of the surface'
-    );
-    expect(() => RoverController.initialize(Position.create(18, 12, orientation), surface)).toThrow(
-      'The Rover is positioned outside the boundaries of the surface'
-    );
-    expect(() => RoverController.initialize(Position.create(23, 12, orientation), surface)).toThrow(
-      'The Rover is positioned outside the boundaries of the surface'
-    );
-    expect(() => RoverController.initialize(Position.create(-1, 8, orientation), surface)).toThrow(
-      'The Rover is positioned outside the boundaries of the surface'
-    );
-    expect(() => RoverController.initialize(Position.create(12, -2, orientation), surface)).toThrow(
-      'The Rover is positioned outside the boundaries of the surface'
-    );
-    expect(() => RoverController.initialize(Position.create(-1, -2, orientation), surface)).toThrow(
-      'The Rover is positioned outside the boundaries of the surface'
-    );
+    expect(() => createRoverAt(22, 0, 'N')).toThrow('The Rover is positioned outside the boundaries of the surface');
+    expect(() => createRoverAt(18, 12, 'N')).toThrow('The Rover is positioned outside the boundaries of the surface');
+    expect(() => createRoverAt(23, 12, 'N')).toThrow('The Rover is positioned outside the boundaries of the surface');
+    expect(() => createRoverAt(-1, 8, 'N')).toThrow('The Rover is positioned outside the boundaries of the surface');
+    expect(() => createRoverAt(12, -2, 'N')).toThrow('The Rover is positioned outside the boundaries of the surface');
+    expect(() => createRoverAt(-1, -2, 'N')).toThrow('The Rover is positioned outside the boundaries of the surface');
   });
 
   test('should rotate to the left', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('N');
-    const position = Position.create(0, 0, orientation);
-    const rover = RoverController.initialize(position, surface);
+    const rover = createRoverAt(0, 0, 'N');
     rover.command('L');
     expect(rover.displayPosition()).toBe('0:0:W');
     rover.command('L');
@@ -58,12 +39,7 @@ describe('The RoverController', () => {
   });
 
   test('should rotate to the right', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('N');
-    const position = Position.create(0, 0, orientation);
-    const rover = RoverController.initialize(position, surface);
+    const rover = createRoverAt(0, 0, 'N');
     rover.command('R');
     expect(rover.displayPosition()).toBe('0:0:E');
     rover.command('R');
@@ -75,12 +51,7 @@ describe('The RoverController', () => {
   });
 
   test('should move forward and backward inside the boundaries of the surface along the y axis', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('N');
-    const position = Position.create(0, 0, orientation);
-    const rover = RoverController.initialize(position, surface);
+    const rover = createRoverAt(0, 0, 'N');
     rover.command('F');
     expect(rover.displayPosition()).toBe('0:1:N');
     rover.command('B');
@@ -92,12 +63,7 @@ describe('The RoverController', () => {
   });
 
   test('should move forward and backward inside the boundaries of the surface along the x axis', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('E');
-    const position = Position.create(0, 0, orientation);
-    const rover = RoverController.initialize(position, surface);
+    const rover = createRoverAt(0, 0, 'E');
     rover.command('F');
     expect(rover.displayPosition()).toBe('1:0:E');
     rover.command('B');
@@ -109,12 +75,7 @@ describe('The RoverController', () => {
   });
 
   test('should move forward and backward wrapping the surface when overlap the boundaries of the surface along the y axis', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('N');
-    const position = Position.create(0, 8, orientation);
-    const rover = RoverController.initialize(position, surface);
+    const rover = createRoverAt(0, 8, 'N');
     rover.command('FFF');
     expect(rover.displayPosition()).toBe('0:1:N');
     rover.command('BBBBB');
@@ -122,12 +83,7 @@ describe('The RoverController', () => {
   });
 
   test('should move forward and backward wrapping the surface when overlap the boundaries of the surface along the x axis', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('E');
-    const position = Position.create(18, 0, orientation);
-    const rover = RoverController.initialize(position, surface);
+    const rover = createRoverAt(18, 0, 'E');
     rover.command('FFF');
     expect(rover.displayPosition()).toBe('1:0:E');
     rover.command('BBBBB');
