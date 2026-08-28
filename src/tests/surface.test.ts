028c2ff7de844d3ad4725ffc831b261304513105
JManuel Velasco <jmanuel.velasco@gmail.com>
2026-08-29 00:55:13 +0200

    parametrizar los tests de Surface

--- src/tests/surface.test.ts
+++ src/tests/surface.test.ts
@@ -4,47 +4,20 @@ import { Position } from '../core/position';
 import { Orientation } from '../core/orientation';
 
 describe('The Surface module', () => {
-  test('returns the normalized position', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('N');
+  const surface = new Surface(10, 20);
+  const orientation = Orientation.create('N');
 
-    const position = Position.create(23, 12, orientation);
+  test.each([
+    [23, 12, 3, 2],
+    [20, 10, 0, 0],
+    [22, 9, 2, 9],
+    [18, 12, 18, 2],
+    [18, -1, 18, 9],
+    [-1, -1, 19, 9],
+    [-3, 3, 17, 3],
+  ])('normalizes the position (%i, %i) to (%i, %i)', (initX, initY, normX, normY) => {
+    const position = Position.create(initX, initY, orientation);
     const normalizedPosition = surface.normalizedPosition(position);
-    expect(normalizedPosition.value()).toEqual({ x: 3, y: 2, orientation: orientation });
-  });
-
-  test('returns the normalized position', () => {
-    const surfaceRows = 10;
-    const surfaceCols = 20;
-    const surface = new Surface(surfaceRows, surfaceCols);
-    const orientation = Orientation.create('N');
-    let position: Position;
-    let normalizedPosition: Position;
-
-    position = Position.create(20, 10, orientation);
-    normalizedPosition = surface.normalizedPosition(position);
-    expect(normalizedPosition.value()).toEqual({ x: 0, y: 0, orientation: orientation });
-
-    position = Position.create(22, 9, orientation);
-    normalizedPosition = surface.normalizedPosition(position);
-    expect(normalizedPosition.value()).toEqual({ x: 2, y: 9, orientation: orientation });
-
-    position = Position.create(18, 12, orientation);
-    normalizedPosition = surface.normalizedPosition(position);
-    expect(normalizedPosition.value()).toEqual({ x: 18, y: 2, orientation: orientation });
-
-    position = Position.create(18, -1, orientation);
-    normalizedPosition = surface.normalizedPosition(position);
-    expect(normalizedPosition.value()).toEqual({ x: 18, y: 9, orientation: orientation });
-
-    position = Position.create(-1, -1, orientation);
-    normalizedPosition = surface.normalizedPosition(position);
-    expect(normalizedPosition.value()).toEqual({ x: 19, y: 9, orientation: orientation });
-
-    position = Position.create(-3, 3, orientation);
-    normalizedPosition = surface.normalizedPosition(position);
-    expect(normalizedPosition.value()).toEqual({ x: 17, y: 3, orientation: orientation });
+    expect(normalizedPosition.value()).toEqual({ x: normX, y: normY, orientation: orientation });
   });
 });
