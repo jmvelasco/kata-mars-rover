d46df3fee0fa9a635f792d0c3b48db50734b3486
JManuel Velasco <jmanuel.velasco@gmail.com>
2026-08-29 21:26:01 +0200

    test(green): rover handles multiple commands and full rotation

--- src/core/domain/Rover.ts
+++ src/core/domain/Rover.ts
@@ -6,12 +6,26 @@ export class Rover {
   ) {}
 
   execute(commands: string): string {
-    if (commands === 'L') {
-      this.direction = 'W';
-    }
-    if (commands === 'R') {
-      this.direction = 'E';
-    }
+    commands.split('').forEach(command => this.processCommand(command));
     return `${this.x}:${this.y}:${this.direction}`;
+  }
+
+  private processCommand(command: string): void {
+    if (command === 'L') this.rotateLeft();
+    if (command === 'R') this.rotateRight();
+  }
+
+  private rotateLeft(): void {
+    if (this.direction === 'N') this.direction = 'W';
+    else if (this.direction === 'W') this.direction = 'S';
+    else if (this.direction === 'S') this.direction = 'E';
+    else if (this.direction === 'E') this.direction = 'N';
+  }
+
+  private rotateRight(): void {
+    if (this.direction === 'N') this.direction = 'E';
+    else if (this.direction === 'E') this.direction = 'S';
+    else if (this.direction === 'S') this.direction = 'W';
+    else if (this.direction === 'W') this.direction = 'N';
   }
 }
