78510d643cd9bb08622d038b68682a51fcba70d8
JManuel Velasco <jmanuel.velasco@gmail.com>
2026-08-29 21:33:45 +0200

    test(green): planet detects obstacles

--- src/core/domain/Planet.ts
+++ src/core/domain/Planet.ts
@@ -2,6 +2,12 @@ export class Planet {
   constructor(
     public readonly width: number,
     public readonly height: number,
-    public readonly obstacles: { x: number; y: number }[]
+    public readonly obstacles: Coordinate[]
   ) {}
+
+  hasObstacleAt(coordinate: Coordinate): boolean {
+    return this.obstacles.some(
+      (obstacle) => obstacle.x === coordinate.x && obstacle.y === coordinate.y
+    );
+  }
 }
