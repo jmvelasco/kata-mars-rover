80888a549f62d977b64713457848ef8680395a07
JManuel Velasco <jmanuel.velasco@gmail.com>
2026-08-29 21:34:24 +0200

    test(green): navigator aborts move if there is an obstacle

--- src/core/domain/Navigator.ts
+++ src/core/domain/Navigator.ts
@@ -2,9 +2,11 @@ import { Planet } from './Planet';
 
 export type Coordinate = { x: number; y: number };
 
-export type NavigationResult =
-  | { success: true; coordinate: Coordinate }
-  | { success: false; reason: 'OBSTACLE'; obstacleCoordinate: Coordinate };
+export type NavigationResult = {
+  success: boolean;
+  coordinate: Coordinate;
+  reason?: 'OBSTACLE';
+};
 
 export class Navigator {
   constructor(private readonly planet: Planet) {}
@@ -23,6 +25,10 @@ export class Navigator {
 
     if (x >= this.planet.width) x = 0;
     else if (x < 0) x = this.planet.width - 1;
+
+    if (this.planet.hasObstacleAt({ x, y })) {
+      return { success: false, coordinate: currentCoordinate, reason: 'OBSTACLE' };
+    }
 
     return { success: true, coordinate: { x, y } };
   }
