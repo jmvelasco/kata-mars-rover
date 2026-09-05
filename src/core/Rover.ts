9287adb5a260a22497d074960229da5c41f2d30b
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-09-05 23:29:15 +0200

    test(green): the rover reports the position it was placed at

--- src/core/Rover.ts
+++ src/core/Rover.ts
@@ -1,9 +1,11 @@
+import { Coordinates } from './Coordinates';
+import { Direction } from './Direction';
 import { Position } from './Position';
 
 export class Rover {
   constructor(public readonly landingPosition: Position) {}
 
-  position(): Position | null {
-    return null;
+  position(): Position {
+    return new Position(new Coordinates(0, 0), Direction.North);
   }
 }
