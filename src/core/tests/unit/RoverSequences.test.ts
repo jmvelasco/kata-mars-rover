3af7e325e8a8777565e0bc499c711d9612dd24c8
Jose Manuel Velasco Martinez <josemanuel.velasco@globant.com>
2026-09-05 23:35:26 +0200

    test(green): a sequence is executed command by command in order

--- src/core/tests/unit/RoverSequences.test.ts
+++ src/core/tests/unit/RoverSequences.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from '@jest/globals';
+import { Command } from '../../Command';
 import { Coordinates } from '../../Coordinates';
 import { Direction } from '../../Direction';
 import { Position } from '../../Position';
@@ -11,5 +12,19 @@ describe('The Rover', () => {
     rover.execute([]);
 
     expect(rover.position()).toEqual(new Position(new Coordinates(1, 3), Direction.West));
+  });
+
+  it('executes the commands of a sequence in the order they were given', () => {
+    const rover = new Rover(new Position(new Coordinates(0, 0), Direction.North));
+
+    rover.execute([
+      Command.MoveForward,
+      Command.MoveForward,
+      Command.TurnRight,
+      Command.MoveForward,
+      Command.MoveForward,
+    ]);
+
+    expect(rover.position()).toEqual(new Position(new Coordinates(2, 2), Direction.East));
   });
 });
